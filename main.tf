616493c8c228e3d33b9f9d01827e67627cb203b7
Muhammad Ahsan Ijaz <ahsanch116@gmail.com>
2024-08-21 22:03:22 -0700

    main.tf
    This Terraform configuration file sets up an EC2 instance in a public subnet within a VPC in the eu-north-1 region:

---
+++ main.tf
@@ -0,0 +1,82 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "MyVPC"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "PublicSubnet"
+  }
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "MyInternetGateway"
+  }
+}
+
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+resource "aws_security_group" "my_security_group" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "MySecurityGroup"
+  }
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-0a44bcbb8b18f238d" # AMI ID for Amazon Linux 2023 in eu-north-1
+  instance_type = "t4g.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+  security_groups = [aws_security_group.my_security_group.name]
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
